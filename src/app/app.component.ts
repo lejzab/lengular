0805e2fd83cc31a52a954768551dd09eac91d6a3
Paweł Bogaczewicz <0x645@tutanota.com>
2024-06-24 20:10:53 +0200

    aah

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [HeaderComponent]
 })
 export class AppComponent {}
