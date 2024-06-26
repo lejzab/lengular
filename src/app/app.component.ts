31612d9080a32cdb9676b563cfc80e3a8c6036ca
Paweł Bogaczewicz <0x645@tutanota.com>
2024-06-26 14:14:52 +0200

    exercise moja wersja

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,23 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUser!: string;
+  selectedUserId!: string;
+  selectedUserName!: string;
 
-onSelectUser(id: string) {
-  console.log(id);
-  this.selectedUser = id
-}
+  onSelectUser(id: string) {
+    console.log(id);
+    this.selectedUserId = id;
+    this.selectedUserName = DUMMY_USERS.find(user => user.id === this.selectedUserId)!.name;
+  }
 }
